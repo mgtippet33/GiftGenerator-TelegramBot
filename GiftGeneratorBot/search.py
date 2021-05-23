e464c20f81b7c0e7e615625b1fcc3cf335a7276a
AlphaFeniks <ivan.korobov@nure.ua>
2021-05-24 01:19:56 +0300

    Corrected text

--- GiftGeneratorBot/search.py
+++ GiftGeneratorBot/search.py
@@ -16,14 +16,14 @@ def searchGift(criteria: UserCriteria):
                           "holiday": criteria.holiday,
                           "interests": ', '.join(criteria.interests)
                       })
-    result = "Упс, щось пішло не так.\nДавай спробуємо підібрати подарунок заново\nНатискай /start"
+    result = "Упс, щось пішло не так.\U0001F614\nДавай спробуємо підібрати подарунок заново\U0001F501"
     if response.status_code == 200:
         json_response = response.json()
-        result = "Знайдені варіанті\n\n"
+        result = "\U0001F539Знайдені варіанти:\n\n"
         for item in json_response['data']:
-            result += "Назва: " + item['name'] + '\n'
-            result += f"<a href=\"{item['link']}\">Посилання на подарунок</a>\n"
-            result += "Ціна: " + str(item['price']) + 'грн\n'
-            result += "Рейтинг подарунку: " + str(item['rate']) + '%\n'
+            result += "\U0001F538Назва: " + item['name'] + '\n'
+            result += f"<a href=\"{item['link']}\">\U0001F517Посилання на подарунок</a>\n"
+            result += "\U0001F4B5Ціна: " + str(item['price']) + 'грн\n'
+            result += "\U0001F4F6Рейтинг подарунку: " + str(item['rate']) + '%\n'
             result += "\n"
     return result
